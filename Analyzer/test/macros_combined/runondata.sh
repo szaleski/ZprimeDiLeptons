cf01e1cb01fec6068d465f274f64d0fd8e25c5dc
Nicola <nicola.de.filippis@cern.ch>
2015-12-30 15:59:46 -0600

    Setup for FNAL

--- Analyzer/test/macros_combined/runondata.sh
+++ Analyzer/test/macros_combined/runondata.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# mkdir -p histo/ExpressPhysics_Run2015B/
 
-# for file in `ls  Data2012/ExpressPhysics_Run2012C-PromptReco-v2/res/*.root`; do
-for file in `cat a.txt`; do
+for file in `ls  /eos/uscms/store/user/cmsdas/2016/LONG_EXERCISES/ZprimeDiLeptons/Data2015_ZprimeMuMu_13TeV_merged/SingleMuon_Run2015B-16Oct2015-v1_Nov13_50ns.root`; do
  echo $file
  basefile=`basename ${file}`;
 
  cat runondata.C | sed "s?filename?${file}?g" > tmp.C
  g++ -I $ROOTSYS/include tmp.C ZprimeMuMuPat.C `root-config --glibs` `root-config --libs` `root-config --cflags` -o runondata
  ./runondata
-
-# mv CMSSW745-Analyse_ZprimeToMuMu_13TeV.root histo/ExpressPhysics_Run2015B/histo_${basefile}
 
 done
 
