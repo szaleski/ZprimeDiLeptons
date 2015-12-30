b8de9572d09f5afe36de94849f53ebf26051edc0
Nicola <nicola.de.filippis@cern.ch>
2015-12-30 16:41:37 -0600

    Fix

--- Analyzer/test/macros_combined/loopsubmit_data.sh
+++ Analyzer/test/macros_combined/loopsubmit_data.sh
@@ -29,32 +29,32 @@ mkdir -p jobs;
 n=0;
 m=0;
 
-echo "Reading data_input_$3_AN.txt file"
+echo "Reading data_input_$2_AN.txt file"
 
-cp -f data_input_$3_AN.txt data_input.txt
-nlines=`wc -l data_input_$3_AN.txt | awk '{print $1}'`;
+cp -f data_input_$2_AN.txt data_input.txt
+nlines=`wc -l data_input_$2_AN.txt | awk '{print $1}'`;
 
 while [ $n -lt ${nlines} ]; do
   (( n = n + 1 ))
   (( m = ${nlines} - n ))
   echo $n $m
-  mkdir -p DataCards$3
-  rm -f DataCards$3/data_input_${n}.txt
-  cat data_input.txt | head -1 > DataCards$3/data_input_${n}.txt
-  samplename=`cat DataCards$3/data_input_${n}.txt | awk '{print $1}'`
+  mkdir -p DataCards$2
+  rm -f DataCards$2/data_input_${n}.txt
+  cat data_input.txt | head -1 > DataCards$2/data_input_${n}.txt
+  samplename=`cat DataCards$2/data_input_${n}.txt | awk '{print $1}'`
   echo $samplename
   cat data_input.txt | tail -n $m >  data_input_tmp.txt
   mv  data_input_tmp.txt data_input.txt
   rm -f jobs/submit_ZprimeMuMuAnalysis_${samplename}.shx
   if [ $4 = ${SCERN} ]; then
-      cat submit_ZprimeMuMuAnalysis_CERN.sh | sed "s?which?data?g" | sed "s?site?$1?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?ZprimeMuMuAnalysis?RunZprimeMuMuAnalysis?g" | sed "s?jobdir?jobs/jobsZprimeMuMu?g" | sed "s?histodir?histos/histosZprimeMuMu?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?DataCards$3/data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
+      cat submit_ZprimeMuMuAnalysis_CERN.sh | sed "s?which?data?g" | sed "s?site?$1?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?ZprimeMuMuAnalysis?RunZprimeMuMuAnalysis?g" | sed "s?jobdir?jobs/jobsZprimeMuMu?g" | sed "s?histodir?histos/histosZprimeMuMu?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?DataCards$2/data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
   elif  [ $4 = ${SFNAL} ]; then 
       cat submit_ZprimeMuMuAnalysis_FNAL.sh  | sed "s?path?$PATH?g"  | sed "s?lib?$LD_LIBRARY_PATH?g" | sed "s?which?data?g" | sed "s?site?$4?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?jobdir?jobs/jobsZprimeMuMu?g" | sed "s?histodir?histos/histosZprimeMuMu?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
-      cat condor_template.cfg  | sed "s?submit_ZprimeMuMuAnalysis_FNAL?submit_ZprimeMuMuAnalysis_${samplename}?g" | sed "s?sig_input_h150.txt?DataCards$3/data_input_${n}.txt?g" | sed "s?mail?`whoami`?g" > jobs/condor_ZprimeMuMuAnalysis_${samplename}.cfg      
+      cat condor_template.cfg  | sed "s?submit_ZprimeMuMuAnalysis_FNAL?submit_ZprimeMuMuAnalysis_${samplename}?g" | sed "s?sig_input_h150.txt?DataCards$2/data_input_${n}.txt?g" | sed "s?mail?`whoami`?g" > jobs/condor_ZprimeMuMuAnalysis_${samplename}.cfg      
   elif  [ $4 = ${SDESY} ]; then
-     cat submit_ZprimeMuMuAnalysis_DESY.sh | sed "s?which?data?g" | sed "s?site?$1?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?ZprimeMuMuAnalysis?RunZprimeMuMuAnalysis?g" | sed "s?jobdir?jobs/jobsZprimeMuMu?g" | sed "s?histodir?histos/histosZprimeMuMu?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?DataCards$3/data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
+     cat submit_ZprimeMuMuAnalysis_DESY.sh | sed "s?which?data?g" | sed "s?site?$1?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?ZprimeMuMuAnalysis?RunZprimeMuMuAnalysis?g" | sed "s?jobdir?jobs/jobsZprimeMuMu?g" | sed "s?histodir?histos/histosZprimeMuMu?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?DataCards$2/data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
   else
-      cat submit_ZprimeMuMuAnalysis.sh | sed "s?which?data?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?jobdir?jobs/jobsZprimeMuMu_combined?g" | sed "s?histodir?histos/histosZprimeMuMu_combined?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?DataCards$3/data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
+      cat submit_ZprimeMuMuAnalysis.sh | sed "s?which?data?g" | sed "s?mc?$3?g" |sed "s?year?$2?g" | sed "s?jobdir?jobs/jobsZprimeMuMu_combined?g" | sed "s?histodir?histos/histosZprimeMuMu_combined?g" | sed "s?output?output_${samplename}?g" | sed "s?data_input.txt?DataCards$2/data_input_${n}.txt?g" | sed "s?s.log?s_${samplename}.log?g" > jobs/submit_ZprimeMuMuAnalysis_${samplename}.sh
 
   fi
 
@@ -67,7 +67,7 @@ while [ $n -lt ${nlines} ]; do
       bsub -q 8nh  submit_ZprimeMuMuAnalysis_${samplename}.sh
   elif  [ $4 = ${SFNAL} ]; then
       echo "Submitting jobs via CONDOR at FNAL"
-      condor_submit  condor_ZprimeMuMuAnalysis_${samplename}.cfg
+      # condor_submit  condor_ZprimeMuMuAnalysis_${samplename}.cfg
   elif  [ $4 = ${SDESY} ]; then
       echo "Submitting jobs via SGE"
       qsub submit_ZprimeMuMuAnalysis_${samplename}.sh   
