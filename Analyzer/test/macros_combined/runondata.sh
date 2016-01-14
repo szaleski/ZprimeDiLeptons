909ed57352c4d30d8066d58c7128e5859aa82355
Nicola <nicola.de.filippis@cern.ch>
2016-01-14 16:48:51 -0600

    Fix

--- Analyzer/test/macros_combined/runondata.sh
+++ Analyzer/test/macros_combined/runondata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for file in `ls  /eos/uscms/store/user/cmsdas/2016/LONG_EXERCISES/ZprimeDiLeptons/Data2015_ZprimeMuMu_13TeV_merged/SingleMuon_Run2015B-16Oct2015-v1_Nov13_50ns.root`; do
+for file in `echo root://cmseos.fnal.gov///store/user/cmsdas/2016/LONG_EXERCISES/ZprimeDiLeptons/Data2015_ZprimeMuMu_13TeV_merged/SingleMuon_Run2015B-16Oct2015-v1_Nov13_50ns.root`; do
  echo $file
  basefile=`basename ${file}`;
 
