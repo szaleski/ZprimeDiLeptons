ccf846975ae9b7c5ddd06d77402ca922356371f8
Nicola <nicola.de.filippis@cern.ch>
2016-03-04 06:58:11 +0100

    Update

--- Analyzer/test/macros_combined/submit_ZprimeMuMuAnalysis.sh
+++ Analyzer/test/macros_combined/submit_ZprimeMuMuAnalysis.sh
@@ -3,14 +3,14 @@
 echo "Processing on " `hostname` "at " `date` 
 
 mkdir -p /home/tmp/defilip/$$
-mkdir -p /lustre/cms/store/user/defilip/ZprimeAnalysis/jobdir
-mkdir -p /lustre/cms/store/user/defilip/ZprimeAnalysis/histodir
+mkdir -p /lustre/cms/store/user/defilip/ZprimeAnalysis76x/jobdir
+mkdir -p /lustre/cms/store/user/defilip/ZprimeAnalysis76x/histodir
 
 workdir=${PWD}
 echo "Running ZprimeMuMu Analysis with executables RunZprimeMuMuAnalysis"
 source /cmshome/nicola/slc6/logincms_cvmfs_slc6.sh
-export SCRAM_ARCH=slc5_amd64_gcc481
-exedir=`echo /cmshome/nicola/slc6/zprime/Analysis13TeV/CMSSW_7_4_5/src/MyCodeArea/Analyzer/test/macros_combined`
+export SCRAM_ARCH=slc6_amd64_gcc491
+exedir=`echo /cmshome/nicola/slc6/zprime/Analysis13TeV/CMSSW_7_6_3/src/ZprimeDiLeptons/Analyzer/test/macros_combined`
 cd ${exedir}
 eval `scramv1 runtime -sh`
 
@@ -19,16 +19,16 @@ if [ -d "/home/tmp/defilip/$$" ]; then
     cd ${workdir};
 fi
 
-savedir=`echo /lustre/cms/store/user/defilip/ZprimeAnalysis/histodir`
+savedir=`echo /lustre/cms/store/user/defilip/ZprimeAnalysis76x/histodir`
 
 echo "Working dir is $workdir"
 echo "Executable dir is $exedir"
 echo "Saving dir is $savedir"
 
 ${exedir}/RunZprimeMuMuAnalysis which ${exedir}/sig_input.txt 1 ${exedir}/bkg_input.txt 1 ${exedir}/data_input.txt 1 Bari year mc >& ${workdir}/RunZprimeMuMuAnalysis.log 
-cp -f ${workdir}/RunZprimeMuMuAnalysis.log /lustre/cms/store/user/defilip/ZprimeAnalysis/jobdir/.
-cp -f ${workdir}/CMSSW745-Analyse_ZprimeToMuMu_13TeV.root    ${savedir}/output.root
-cp -f ${workdir}/CMSSW745-Analyse_ZprimeToMuMu_13TeV_cand.txt ${savedir}/output_cand.txt
+cp -f ${workdir}/RunZprimeMuMuAnalysis.log /lustre/cms/store/user/defilip/ZprimeAnalysis76x/jobdir/.
+cp -f ${workdir}/CMSSW763-Analyse_ZprimeToMuMu_13TeV.root    ${savedir}/output.root
+cp -f ${workdir}/CMSSW763-Analyse_ZprimeToMuMu_13TeV_cand.txt ${savedir}/output_cand.txt
 # cleaning the worker node
 if [ -d "/home/tmp/defilip/$$" ]; then
     rm -f -R *
