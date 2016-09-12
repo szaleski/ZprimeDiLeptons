c0efb24cd8bc9d13442651504e7c5ea04c53c061
Nicola <nicola.de.filippis@cern.ch>
2016-09-12 16:05:36 +0200

    Fix

--- Analyzer/test/macros_miniaod/submit_ZprimeMuMuAnalysis_DESY.sh
+++ Analyzer/test/macros_miniaod/submit_ZprimeMuMuAnalysis_DESY.sh
@@ -2,11 +2,13 @@
 
 echo "Processing on " `hostname` "at " `date` 
 
-mkdir -p $$
 mkdir -p /nfs/dust2/cms/group/DAS2016/${USER}/jobdir
 mkdir -p /nfs/dust2/cms/group/DAS2016/${USER}/histodir
 
-workdir=${PWD}
+cd /tmp
+mkdir $$
+workdir=${PWD}/$$
+
 echo "Running ZprimeMuMu Analysis with executables RunZprimeMuMuAnalysis"
 source /cvmfs/cms.cern.ch/cmsset_default.sh 
 export SCRAM_ARCH=slc6_amd64_gcc530
@@ -14,10 +16,7 @@ exedir=`echo /afs/desy.de/user/s/school22/CMSSW_8_0_13/src/ZprimeDiLeptons/Analy
 cd ${exedir}
 eval `scramv1 runtime -sh`
 
-if [ -d "${PWD}/$$" ]; then
-    workdir=`echo ${PWD}/$$`;
-    cd ${workdir};
-fi
+cd ${workdir};
 
 savedir=`echo /nfs/dust2/cms/group/DAS2016/${USER}/histodir`
 
@@ -30,7 +29,8 @@ cp -f ${workdir}/RunZprimeMuMuAnalysis.log /nfs/dust2/cms/group/DAS2016/${USER}/
 cp -f ${workdir}/CMSSW803-Analyse_ZprimeToMuMu_13TeV.root     ${savedir}/output.root
 cp -f ${workdir}/CMSSW803-Analyse_ZprimeToMuMu_13TeV_cand.txt ${savedir}/output_cand.txt
 # cleaning the worker node
-if [ -d "${PWD}/$$" ]; then
-    rm -f -R *
-    rm -f *
-fi
+
+#if [ -d "${workdir}" ]; then
+#    rm -f -R *
+#    rm -f *
+#fi
