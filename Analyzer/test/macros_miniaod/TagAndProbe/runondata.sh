331b178ede7b63689a86beedbb9b085489f759c9
Nicola <nicola.de.filippis@cern.ch>
2016-09-13 23:38:25 +0200

    Fix

--- Analyzer/test/macros_miniaod/TagAndProbe/runondata.sh
+++ Analyzer/test/macros_miniaod/TagAndProbe/runondata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-for file in `echo /lustre/cms/store/user/defilip/ZprimeAnalysis/Data2016_ZprimeMuMu_13TeV_merged_HLT/CMSSW_8_0_13_ZPRIMEMuMu_13TeV-DataG-V1-JSON-tree.root`; do
+# for file in `echo /lustre/cms/store/user/defilip/ZprimeAnalysis/Data2016_ZprimeMuMu_13TeV_merged_HLT/CMSSW_8_0_13_ZPRIMEMuMu_13TeV-DataG-V1-JSON-tree.root`; do
+for file in `echo /lustre/cms/store/user/defilip/ZprimeAnalysis/Spring16_ZprimeMuMu_13TeV_merged_HLT/CMSSW8012_MC_reHLT_DYtoMuMu120to200_13TeV_tree.root`; do
  echo $file
  basefile=`basename ${file}`;
 
