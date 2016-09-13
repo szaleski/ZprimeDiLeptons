e03bd2ba7a6be3e67cf4864c15d79638ea90dd37
Nicola <nicola.de.filippis@cern.ch>
2016-09-13 23:48:57 +0200

    Fix

--- Analyzer/test/FakeRate_25ns/script.sh
+++
@@ -1,8 +0,0 @@
-
-
-for file in `ls /lustre/cms/store/user/defilip/ZprimeAnalysis/Spring16_merged/QCD*`; do
- echo $file
- basefile=`basename $file`
- echo $basefile
- mv /lustre/cms/store/user/defilip/ZprimeAnalysis/Spring16_merged/$basefile /lustre/cms/store/user/defilip/ZprimeAnalysis/Spring16_merged/CMSSW803_MC_$basefile
-done
