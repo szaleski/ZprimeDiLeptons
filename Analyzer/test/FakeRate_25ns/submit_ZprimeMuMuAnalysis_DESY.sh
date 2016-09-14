7ed3e0dc2974c5745454492458bcdffff74859c4
Nicola <nicola.de.filippis@cern.ch>
2016-09-14 11:31:24 +0200

    Fix

--- Analyzer/test/FakeRate_25ns/submit_ZprimeMuMuAnalysis_DESY.sh
+++ Analyzer/test/FakeRate_25ns/submit_ZprimeMuMuAnalysis_DESY.sh
@@ -25,7 +25,7 @@ echo "Executable dir is $exedir"
 echo "Saving dir is $savedir"
 
 ${exedir}/RunZprimeMuMuAnalysis_FR which ${exedir}/sig_input.txt 1 ${exedir}/bkg_input.txt 1 ${exedir}/data_input.txt 1 site year mc >& ${workdir}/RunZprimeMuMuAnalysis_FR.log 
-cp -f ${workdir}/RunZprimeMuMuAnalysis_FR.log /nfs/dust2/cms/group/DAS2016/${USER}/jobdir/.
+cp -f ${workdir}/RunZprimeMuMuAnalysis_FR.log /nfs/dust2/cms/group/DAS2016/${USER}/jobdir/output.log
 cp -f ${workdir}/CMSSW803-Analyse_ZprimeToMuMu_13TeV.root     ${savedir}/output.root
 cp -f ${workdir}/CMSSW803-Analyse_ZprimeToMuMu_13TeV_cand.txt ${savedir}/output_cand.txt
 # cleaning the worker node
